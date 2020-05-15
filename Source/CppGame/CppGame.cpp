186b705f2a27759ded7243f0f8d637f4ad516d10
SorenG <soren.gilbertson13@gmail.com>
2020-05-15 08:24:55 -0600

    Project Files

---
+++ Source/CppGame/CppGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CppGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CppGame, "CppGame" );
