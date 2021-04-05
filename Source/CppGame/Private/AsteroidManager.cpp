ca263c58f4e5ea2811df9f677d123c7a425ee293
SorenG <soren.gilbertson13@gmail.com>
2021-04-05 10:54:21 -0600

    Solar Particles: CME Emitter

--- Source/CppGame/Private/AsteroidManager.cpp
+++ Source/CppGame/Private/AsteroidManager.cpp
@@ -15,7 +15,8 @@ AAsteroidManager::AAsteroidManager()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
-	shapeGenerator = new ShapeGenerator();}
+	shapeGenerator = new ShapeGenerator();
+}
 
 // Called when the game starts or when spawned
 void AAsteroidManager::BeginPlay()
